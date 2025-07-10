a9c7c348f1a58c0efb1366ea33d3d3b9d9d0df7c
Udit-Mahesh <uditsmahesh@gmail.com>
2025-07-10 19:15:23 +0530

    keep current changes

--- static/js/common.js
+++ static/js/common.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function callApi(method,url,data){
     $.ajax({
         type:method,
@@ -13,19 +12,3 @@ function callApi(method,url,data){
     })
 }
 
-=======
-function callApi(method,url,data){
-    $.ajax({
-        type:method,
-        url:url,
-        data:data,
-        contentType:'application/json',
-        error:(xhr)=>{
-            alert(xhr.responseJSON?.message || 'An unknown error occured');
-        }
-    }).done(()=>{
-        window.location.reload();
-    })
-}
-
->>>>>>> 021fdf591c973b00f047735ae9a78bf0621bacba
--- static/js/staff.js
+++ static/js/staff.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $('#add-staff-save').click(()=>{
     let data=$('#addStaffForm').serializeArray();
     if (data[3].value===data[4].value){
@@ -22,29 +21,4 @@ $('#delete-staff-save').click(()=>{
         staff_name : data[1].value
     };
     callApi('POST','http://127.0.0.1:5000/deleteStaff',JSON.stringify(requestPayload));
-=======
-$('#add-staff-save').click(()=>{
-    let data=$('#addStaffForm').serializeArray();
-    if (data[3].value===data[4].value){
-        let requestPayload={
-            staff_name : data[0].value,
-            role : data[1].value,
-            email : data[2].value,
-            password : data[3].value,
-            phone_no : data[5].value
-        };
-        callApi('POST','http://127.0.0.1:5000/addStaff',JSON.stringify(requestPayload));
-    }else{
-        alert('Enter matching passwords');
-    }
-});
-
-$('#delete-staff-save').click(()=>{
-    let data=$('#deleteStaffForm').serializeArray();
-    let requestPayload={
-        staff_id : data[0].value,
-        staff_name : data[1].value
-    };
-    callApi('POST','http://127.0.0.1:5000/deleteStaff',JSON.stringify(requestPayload));
->>>>>>> 021fdf591c973b00f047735ae9a78bf0621bacba
 })
