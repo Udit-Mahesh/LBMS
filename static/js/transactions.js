18c5bd936b96d3740497c63c7e5e3396f4559dcf
Udit-Mahesh <uditsmahesh@gmail.com>
2025-07-10 19:02:26 +0530

    keep

--- static/js/transactions.js
+++ static/js/transactions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $('#borrow-book-save').click(()=>{
     let data=$('#borrowForm').serializeArray();
     let requestPayload={
@@ -15,22 +14,4 @@ $('#return-book-save').click(()=>{
         member_id : data[1].value
     };
     callApi('POST','http://127.0.0.1:5000/returnBook',JSON.stringify(requestPayload));
-=======
-$('#borrow-book-save').click(()=>{
-    let data=$('#borrowForm').serializeArray();
-    let requestPayload={
-        book_id:data[0].value,
-        member_id:data[1].value
-    };
-    callApi('POST','http://127.0.0.1:5000/borrowBook',JSON.stringify(requestPayload));
-});
-
-$('#return-book-save').click(()=>{
-    let data=$('#returnForm').serializeArray();
-    let requestPayload={
-        book_id : data[0].value,
-        member_id : data[1].value
-    };
-    callApi('POST','http://127.0.0.1:5000/returnBook',JSON.stringify(requestPayload));
->>>>>>> 021fdf591c973b00f047735ae9a78bf0621bacba
 })
